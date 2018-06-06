6264776b64d496443a8adaefcac9612e8c32d78a
John Fu <johnfu@ISLs-iMac.local>
2018-06-06 16:34:30 +1200

    to me

--- Julian Garnier_files/scripts.js
+++ Julian Garnier_files/scripts.js
@@ -26,7 +26,7 @@ window.onload = function() {
     'I\'m Qiang',
     'I design and code things on the web',
     'I\'m currently looking for full time web developer role.<br> You can contact me at <a href="mailto:john.fu1984@gmail.com">john.fu1984@gmail.com</a>',
-    '<a target="_blank" href="https://twitter.com/juliangarnier">twitter.com/juliangarnier</a><br><a target="_blank" href="https://codepen.io/juliangarnier">codepen.io/juliangarnier</a><br><a target="_blank" href="https://github.com/juliangarnier">github.com/juliangarnier</a>',
+    'phone:02108337543<br><a target="_blank" href="https://https://www.linkedin.com/in/547557356">Linked in</a><br>',
     getCurrentTime(),
     '👀 J.'
   ]
